7aacb68bebd8a8bf8e9351fe8a84753bdf08b870
Yuran Simao <yuransimao7@gmail.com>
2023-02-03 14:18:55 +0100

    setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssestPlugin = require('optimize-css-assets-webpack-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+
+
