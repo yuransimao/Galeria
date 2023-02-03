8a2eb16cadbda6d17afc5d79fba47eb4ebf71e9a
Yuran Simao <yuransimao7@gmail.com>
2023-02-03 14:49:14 +0100

    updating settings of the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,19 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssestPlugin = require('optimize-css-assets-webpack-plugin')
 const copyWebpackPlugin = require('copy-webpack-plugin')
 
-
+module.exports ={
+    mode:'developement',
+    entry:'./src/index.js',
+    devServer:{
+        port:9000,
+        static:'./public'
+    },
+    optimization:{
+        minimizer:[
+            new UglifyJsPlugin({
+                cache: true,
+                parallel:true
+            })
+        ]
+    }
+}
