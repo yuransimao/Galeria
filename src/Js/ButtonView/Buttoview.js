f456a68d6d3e39e8b83d288d9ab58cdbcbd463ec
Yuran Simao <yuransimao7@gmail.com>
2023-02-06 11:05:59 +0100

    update

--- src/Js/ButtonView/Buttoview.js
+++ src/Js/ButtonView/Buttoview.js
@@ -1,27 +1,33 @@
-import $ from "jquery"
+import $, { each } from "jquery"
 
 import {onloadSucessgaleyView} from "../Core/includes"
 
 function Calview (){
     
-    $('[wm-view]').each(function(i, e){
-    $(this).on('click', function(){
+    $('[wm-view1]').on('click', function(){
         $('[wm-bla]').addClass('actives')
 
         console.log('ok')
     })
-})}
+    
+    $('[wm-view2]').on('click', function(){
+        $('[wm-bla2]').addClass('actives')
+
+        console.log('ok')
+    })
+}
 
 
 function RemoveView(){
-    $('[wm-RemoveView]').each(
+$('[wm-RemoveView]').each(
 
-        function(i, e){
-            $(this).on('click', function () {
-                $('[wm-bla]').removeClass('actives')
-            })
-        }
-    )
+function(i, e){
+$(this).on('click', function () {
+$(['[wm-bla]','[wm-bla2]']).each(function(i, e){
+$(e).removeClass('actives')})
+})
+}
+)
 }
 
 
